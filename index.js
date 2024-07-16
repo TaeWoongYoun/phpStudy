8d3fea8943e010f6f00290139c7cd8f95638f3f4
xodnd0507 <24457545yong@gmail.com>
2024-07-16 09:31:02 +0900

    test : Update code

--- index.js
+++ index.js
@@ -11,51 +11,58 @@ reset.addEventListener('click', () =>{
     join_form.style.display = 'none'
 })
 
-join_submit.addEventListener('click', () => {
-    const id = document.querySelector('.input_id')
-    const name = document.querySelector('.input_name')
-    const pw = document.querySelector('.input_pw')
-    const pw_ck = document.querySelector('.input_pw_ck')
+join_submit.addEventListener('click', (event) => {
+    const id = document.querySelector('#input_id');
+    const name = document.querySelector('.input_name');
+    const pw = document.querySelector('.input_pw');
+    const pw_ck = document.querySelector('.input_pw_ck');
 
-    if (id.value == ''){
-        alert('아이디를 입력해주세요.')
+    if (!id.value) {
+        alert('아이디를 입력해주세요.');
         id.focus();
-        return false;
+        event.preventDefault();
+        return;
     }
 
-    if (id.value.length < 4 || id.value.length > 12){
-        alert('아이디를 4자 이상 12 이하로 입력해주세요.')
+    if (id.value.length < 4 || id.value.length > 12) {
+        alert('아이디를 4자 이상 12자 이하로 입력해주세요.');
         id.focus();
-        return false;
+        event.preventDefault();
+        return;
     }
 
-    if (name.value == ''){
-        alert('이름을 입력해주세요.')
+    if (!name.value) {
+        alert('이름을 입력해주세요.');
         name.focus();
-        return false;
+        event.preventDefault();
+        return;
     }
 
-    if (name.value.length < 4 || name.value.length > 12){
-        alert('이름을 4자 이상 12 이하로 입력해주세요.')
+    if (name.value.length < 4 || name.value.length > 12) {
+        alert('이름을 4자 이상 12자 이하로 입력해주세요.');
         name.focus();
-        return false;
+        event.preventDefault();
+        return;
     }
 
-    if (pw.value == ''){
-        alert('비밀번호를 입력해주세요.')
+    if (!pw.value) {
+        alert('비밀번호를 입력해주세요.');
         pw.focus();
-        return false;
+        event.preventDefault();
+        return;
     }
 
-    if (pw_ck.value == ''){
-        alert('비밀번호가 일치하지 않습니다. 비밀번호를 확인해주세요');
-        pw_ck.focus();
-        return false;
-    }
-
-    if (pw.value != pw_ck.value){
-        alert('비밀번호가 다릅니다. 다시 입력해주세요');
+    if (pw.value !== pw_ck.value) {
+        alert('비밀번호가 다릅니다. 다시 입력해주세요.');
         pw.focus();
-        return false;
+        event.preventDefault();
+        return;
     }
-})
+
+    if (pw.value.length < 8 || pw.value.length > 16) {
+        alert('비밀번호를 8자 이상 16자 이하로 입력해주세요.');
+        pw.focus();
+        event.preventDefault();
+        return;
+    }
+});
