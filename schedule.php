a896f541e5879ef1f41c1b5715a7cfd1cd1fcaba
xodnd0507 <24457545yong@gmail.com>
2024-07-17 20:55:45 +0900

    test : Create sub page

--- schedule.php
+++ schedule.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>introduce page</title>
+    <title>read page</title>
     <link rel="stylesheet" href="crud.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
@@ -61,27 +61,61 @@
     </header>
     
     <div class="content">
-        <table>
+        <table class="calender">
             <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Title</th>
-                <th scope="col">Name</th>
-                <th scope="col">Date</th>
+                <th style="color: red;">일</th>
+                <th>월</th>
+                <th>화</th>
+                <th>수</th>
+                <th>목</th>
+                <th>금</th>
+                <th style="color: blue;">토</th>
             </tr>
-            
-                <?php
-                    $sql = "SELECT * FROM book";
-                    $result = mysqli_query($conn, $sql);
-                    while ($row = mysqli_fetch_array($result)){
-                        echo "
-                        <tr>
-                            <td>{$row['id']}</td>
-                            <td>{$row['title']}</td>
-                            <td>{$row['name']}</td>
-                            <td>{$row['date']}</td>
-                        </tr>";
-                    }
-                ?>
+            <tr>
+                <td style="color: grey;">31</td>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td style="color: blue;">6</td>
+            </tr>
+            <tr>
+                <td style="color: red;">7</td>
+                <td>8</td>
+                <td>9</td>
+                <td>10</td>
+                <td>11</td>
+                <td>12</td>
+                <td style="color: blue;">13</td>
+            </tr>
+            <tr>
+                <td style="color: red;">14</td>
+                <td>15</td>
+                <td>16</td>
+                <td>17</td>
+                <td>18</td>
+                <td>19</td>
+                <td style="color: blue;">20</td>
+            </tr>
+            <tr>
+                <td style="color: red;">21</td>
+                <td>22</td>
+                <td>23</td>
+                <td>24</td>
+                <td>25</td>
+                <td>26</td>
+                <td style="color: blue;">27</td>
+            </tr>
+            <tr>
+                <td style="color: red;">28</td>
+                <td>29</td>
+                <td>30</td>
+                <td>31</td>
+                <td style="color: grey;">1</td>
+                <td style="color: grey;">2</td>
+                <td style="color: grey;">3</td>
+            </tr>
         </table>
     </div>
 
--- story.php
+++ story.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>introduce page</title>
+    <title>read page</title>
     <link rel="stylesheet" href="crud.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
@@ -61,28 +61,11 @@
     </header>
     
     <div class="content">
-        <table>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Title</th>
-                <th scope="col">Name</th>
-                <th scope="col">Date</th>
-            </tr>
-            
-                <?php
-                    $sql = "SELECT * FROM book";
-                    $result = mysqli_query($conn, $sql);
-                    while ($row = mysqli_fetch_array($result)){
-                        echo "
-                        <tr>
-                            <td>{$row['id']}</td>
-                            <td>{$row['title']}</td>
-                            <td>{$row['name']}</td>
-                            <td>{$row['date']}</td>
-                        </tr>";
-                    }
-                ?>
-        </table>
+        <h4 style="text-align: center;">
+            설립 : 1990년 <br>
+            공사 : 2006년 <br>
+            홈페이지 개설 : 2024년 <br>
+        </h4>
     </div>
 
     <footer>
